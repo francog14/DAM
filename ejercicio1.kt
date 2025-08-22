111541d09b6287bef0b711828b6cb9a66af17c60
fnofranco <fr.garaym@duocuc.cl>
2025-08-22 15:25:14 -0400

    eje1

---
+++ ejercicio1.kt
@@ -0,0 +1,32 @@
+fun main (){
+    println("iNICIO DEL PROGRAMA")
+
+    val a = 5
+    val b = 3
+
+
+    val sum = a+b
+    val res = a-b
+    val mul = a*b
+    val div = (a.toFloat() /b)
+    val mod = a % b
+
+    println("estoy en suma")
+    println("sum: $sum")
+
+    println("estoy en resta")
+    println("res: $res")
+
+    println("estoy en multiplicacion")
+    println("mul: $mul")
+
+    println("estoy en division")
+    println("div: $div")
+
+    println("estoy en modulo")
+    println("mod: $mod")
+
+}
+
+
+
