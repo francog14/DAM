0346c333e61a176c15902572b80e8b3a3d618894
fnofranco <fr.garaym@duocuc.cl>
2025-08-22 15:47:10 -0400

    ejemplo2

--- ejercicio1.kt
+++ ejercicio1.kt
@@ -4,29 +4,33 @@ fun main (){
     val a = 5
     val b = 3
 
+    sumar(a,b)
+    sumar(9, 6)
+    sumar(4, 7)
+    sumar(93, 1)
 
-    val sum = a+b
-    val res = a-b
-    val mul = a*b
-    val div = (a.toFloat() /b)
-    val mod = a % b
+    val x = 5
+    val y = 3
 
-    println("estoy en suma")
-    println("sum: $sum")
+    dividir(x,y)
+    dividir(9, 6)
+    dividir(4, 7)
+    dividir(93, 1)
+}
 
-    println("estoy en resta")
-    println("res: $res")
-
-    println("estoy en multiplicacion")
-    println("mul: $mul")
-
-    println("estoy en division")
-    println("div: $div")
-
-    println("estoy en modulo")
-    println("mod: $mod")
+fun sumar(a:Int, b:Int){
+    print("programa de sumas extremas - voy a sumar $a con $b = ")
+    println(a+b)
 
 }
 
+fun dividir(x: Int, y: Int) {
 
+    if (y != 0) {
+        print("programa de division extremas - voy a dividir $x con $y = ")
+        println(x / y)
+    } else {
+        println("Error: División por cero")
+    }
+}
 
