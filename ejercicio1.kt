8cfe9e33740a69ca3775c51bde24d34fdb9f9daa
fnofranco <fr.garaym@duocuc.cl>
2025-08-27 11:30:45 -0400

    operatorias terminadas

--- ejercicio1.kt
+++ ejercicio1.kt
@@ -9,20 +9,40 @@ fun main (){
     sumar(4, 7)
     sumar(93, 1)
 
+    resta(a,b)
+    resta(9, 6)
+    resta(4, 7)
+    resta(93, 1)
+
     val x = 5
     val y = 3
 
+    multi(x,y)
+    multi(9, 6)
+    multi(4, 0)
+    multi(93, 1)
+
     dividir(x,y)
     dividir(9, 6)
-    dividir(4, 7)
+    dividir(4, 0)
     dividir(93, 1)
 }
 
 fun sumar(a:Int, b:Int){
     print("programa de sumas extremas - voy a sumar $a con $b = ")
     println(a+b)
-
 }
+
+fun resta(a: Int, b: Int){
+    print("programa de restas extremas - voy a restar $a con $b = ")
+    println(a-b)
+}
+
+fun multi(x: Int, y: Int){
+    print("programa de multiplicanciones extremas - voy a multiplicar $x con $y = ")
+    println(x*y)
+}
+
 
 fun dividir(x: Int, y: Int) {
 
