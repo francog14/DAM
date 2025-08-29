d18d1c047ccca743675c8f7853fee7237e3b760c
fnofranco <fr.garaym@duocuc.cl>
2025-08-29 14:54:31 -0400

    lista normal

---
+++ listas.kt
@@ -0,0 +1,11 @@
+fun main (){
+    var lista = listOf(1, 2, 5)
+    println("lista: $lista")
+
+    var conjunto = setOf(1, 2, 5)
+    println("conjunto: $conjunto")
+
+    var mapa = mapOf("nombre" to "juan", "edad" to 9, "dolares" to 1000)
+    println("mapa: $mapa")
+
+}
